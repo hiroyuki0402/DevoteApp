76f690e4e51887a734daa3695e83fbc34c4e025b
hiroyuki0402 <hiro1990.cloud@gmail.comcd>
2023-11-04 19:17:42 +0900

    コミット忘れ

--- Devote/View/Home/Component/BackgroundImageView.swift
+++ Devote/View/Home/Component/BackgroundImageView.swift
@@ -6,3 +6,19 @@
 //
 
 import Foundation
+import SwiftUI
+
+struct BackgroundImageView: View {
+  var body: some View {
+      Image(.rocket)
+      .antialiased(true)
+      .resizable()
+      .scaledToFill()
+      .ignoresSafeArea(.all)
+  }
+}
+
+#Preview {
+    BackgroundImageView()
+  }
+
